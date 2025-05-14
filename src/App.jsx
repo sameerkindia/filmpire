3a7132febcf53654142e4d55601763959a01aed9
sameerkindia <sameerkhanaa5@gmail.com>
2025-05-14 08:48:06 +0530

    redux toolkit store setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { useState } from 'react'
-import './App.css'
-import {CssBaseline} from '@mui/material'
+import "./App.css";
+import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+
+import Movies from "./components/Movies/Movies";
+import MovieInformation from "./components/MovieInformation/MovieInformation";
+import Navbar from "./components/Navbar/Navbar";
+import Actors from "./components/Actors/Actors";
+import Profile from "./components/Profile/Profile";
+// import { Actors, MovieInformation, Movies, Profile } from "./components";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        App
-      </div>  
+      <div className="flex h-full">
+        <CssBaseline />
+        <Navbar />
+        <main className="grow p-8">
+        <div className="mt-[70px]">
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/movie/:id" element={<MovieInformation />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+        </div>
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
